9853e6862651bc0a8f1cd572fde23b123d6ed5f9
Chetan Gaurkhede <gaurkhede732@gmail.com>
2024-11-08 11:16:04 +0530

    push for pull

--- src/Pages/Inventory.jsx
+++ src/Pages/Inventory.jsx
@@ -28,7 +28,7 @@ const Inventory = () => {
         <div className="flex w-full justify-between">
                <IconBreadcrumbs path={"Inventory"} />
           <div>
-            <Link to="/">
+            <Link to="/category">
               <button className="bg-blue-500 p-2 px-4 rounded-xl text-white flex gap-3"><img src="src/assets/Images/fi_plus.png" alt="" /> Add a New Procduct</button>
             </Link>
           </div>
